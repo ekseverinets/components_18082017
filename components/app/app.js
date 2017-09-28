fb38ffff2b0e54b4280c49165cbc63608ae7eeca
Evgeniya Severinets <ekseverinets@gmail.com>
2017-09-28 19:48:20 +0300

    Startув working on css

--- components/app/app.js
+++ components/app/app.js
@@ -1,47 +1,47 @@
 (function() {
-    'use strict';
+	'use strict';
 
-    // import
-    const Menu = window.Menu;
+	// import
+	const Menu = window.Menu;
 
-    class App {
-        constructor ({el}) {
+	class App {
+		constructor ({el}) {
 
-            this.menu = new Menu({
-                el: el.querySelector('.js-menu'),
-                data: {
-                    title: 'Сайты',
-                    items: []
-                }
-            });
+			this.menu = new Menu({
+				el: el.querySelector('.js-menu'),
+				data: {
+					title: 'Сайты',
+					items: []
+				}
+			});
 
-            this.menu.setData({
-                title: 'Сайты',
-                items: [
-                    {title: 'Первый'},
-                    {
-                      title: 'Второй',
-                      items: [
-                        {title: 'Второй-первый'},  
-                        {
-                          title: 'Второй-второй',
-                          items: [
-                            {title: 'Второй-второй-1'},
-                            {title: 'Второй-второй-2'},
-                            {title: 'Второй-второй-3'}
-                          ]
-                        },  
-                      ]
-                    },
-                    {title: 'Третий'},
-                    {title: 'Четвертый'}
-                  ]
-            });
+			this.menu.setData({
+				title: 'Сайты',
+				items: [
+					{title: 'Первый'},
+					{
+						title: 'Второй',
+						items: [
+							{title: 'Второй-первый'},  
+							{
+								title: 'Второй-второй',
+								items: [
+									{title: 'Второй-второй-1'},
+									{title: 'Второй-второй-2'},
+									{title: 'Второй-второй-3'}
+								]
+							},  
+						]
+					},
+					{title: 'Третий'},
+					{title: 'Четвертый'}
+				]
+			});
 
-        }
-    }
+		}
+	}
 
-    // export 
-     window.App = App;
+	// export 
+	window.App = App;
 
 })();
